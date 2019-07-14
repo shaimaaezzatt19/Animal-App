08bf99a89a5b1a460eba63839fb81505b39e85df
shaimaaezzat19 <shaimaaezzat191996@gmail.com>
2019-07-14 16:54:23 +0200

    14-7-2019

--- app/src/main/java/com/khatab/animalapp/ui/activites/MainMenueActivity.java
+++ app/src/main/java/com/khatab/animalapp/ui/activites/MainMenueActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.MediaController;
@@ -17,6 +18,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainMenueActivity extends AppCompatActivity {
+    private final int splash_Display_Length = 40000;
     VideoView simpleVideoView;
     MediaController mediaControls;
     boolean pausing = false;
@@ -25,48 +27,58 @@ public class MainMenueActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menue);
-        ButterKnife.bind(this);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main_menue );
+        ButterKnife.bind( this );
 
 
-        simpleVideoView = (VideoView) findViewById(R.id.main_Vedio);
+        simpleVideoView = (VideoView) findViewById( R.id.main_Vedio );
 
 
         if (mediaControls == null) {
             // create an object of media controller class
-            mediaControls = new MediaController(MainMenueActivity.this);
-            mediaControls.setAnchorView(simpleVideoView);
+            mediaControls = new MediaController( MainMenueActivity.this );
+            mediaControls.setAnchorView( simpleVideoView );
         }
         // set the media controller for video view
-        simpleVideoView.setMediaController(mediaControls);
+        simpleVideoView.setMediaController( mediaControls );
         // set the uri for the video view
-        simpleVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.vediozoom));
+        simpleVideoView.setVideoURI( Uri.parse( "android.resource://" + getPackageName() + "/" + R.raw.vediozoom ) );
         // start a video
         simpleVideoView.start();
 
         // implement on completion listener on video view
-        simpleVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+        simpleVideoView.setOnCompletionListener( new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
-                Toast.makeText(getApplicationContext(), "أهلا بكم في :غنى الشام:", Toast.LENGTH_LONG).show(); // display a toast when an video is completed
+                Toast.makeText( getApplicationContext(), "أهلا بكم في :غنى الشام:", Toast.LENGTH_LONG ).show(); // display a toast when an video is completed
             }
-        });
-        simpleVideoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+        } );
+        simpleVideoView.setOnErrorListener( new MediaPlayer.OnErrorListener() {
             @Override
             public boolean onError(MediaPlayer mp, int what, int extra) {
-                Toast.makeText(getApplicationContext(), "Oops An Error Occur While Playing Video...!!!", Toast.LENGTH_LONG).show(); // display a toast when an error is occured while playing an video
+                Toast.makeText( getApplicationContext(), "Oops An Error Occur While Playing Video...!!!", Toast.LENGTH_LONG ).show(); // display a toast when an error is occured while playing an video
                 return false;
 
             }
-        });
+        } );
 
+
+        new Handler().postDelayed( new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent( MainMenueActivity.this, FullMenueWithIcons.class );
+                MainMenueActivity.this.startActivity( intent );
+                MainMenueActivity.this.finish();
+            }
+        }, splash_Display_Length );
     }
+
 
     @OnClick(R.id.Main_MenuE_munueIcon_IV)
     public void onViewClicked() {
-        Intent i = new Intent(MainMenueActivity.this, FullMenueWithIcons.class);
-        startActivity(i);
+        Intent i = new Intent( MainMenueActivity.this, FullMenueWithIcons.class );
+        startActivity( i );
     }
 }
 
